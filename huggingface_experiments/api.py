75fdace4e12e3048e8454ca8521928a2125776b2
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-06-07 15:07:17 -0500

    First pass at implementing APIs

--- huggingface_experiments/api.py
+++ huggingface_experiments/api.py
@@ -1,8 +1,7 @@
-import json
 from argparse import Namespace
-from email.policy import default
+from pathlib import Path
 
-from requests import delete, post
+from requests import delete, post, put
 from requests.models import Response
 from utils.args import apiArgs
 
@@ -23,18 +22,36 @@ def createModelRepo(token: str, name: str, organization: str = None) -> Response
     return p
 
 
-def deleteModelRepo(token: str, name: str, organization: str = None) -> Response:
+def deleteModelRepo(token: str, name: str, organization: str) -> Response:
     url: str = f"{rootURL}/api/repos/delete"
     headers: dict = {"authorization": f"Bearer {token}"}
-
-    match organization:
-        case None:
-            json: dict = {"name": name}
-        case _:
-            json: dict = {"name": name, "organization": organization}
-
+    json: dict = {"name": name, "organization": organization}
     d: Response = delete(url, headers=headers, json=json)
     return d
+
+def makePrivateModelRepo(token: str, name: str, organization: str, private: str = "private")   ->  Response:
+    url: str = f"{rootURL}/api/repos/model/{organization}/{name}/settings"
+    headers: dict = {"authorization": f"Bearer {token}"}
+    json: dict = {"private": private}
+    mp: Response = put(url, headers=headers, json=json)
+    return mp
+
+def moveModelRepo(token: str, fromRepo: str, toRepo: str)   ->  Response:
+    url: str = f"{rootURL}/api/repos/move"
+    headers: dict = {"authorization": f"Bearer {token}"}
+    json: dict = {"fromRepo": fromRepo, "toRepo": toRepo}
+    m: Response = post(url, headers=headers, json=json)
+    return m
+
+def uploadFileToModelRepo(token: str, filepath: str, name: str, organization: str, revision: str = "1") ->  Response:
+    path: Path = Path(filepath)
+
+    url: str = f"{rootURL}/api/model/{name}/{organization}/upload/{revision}/{path.name}"
+    headers: dict = {"authorization": f"Bearer {token}"}
+    with open(filepath, "rb") as bytesFile:
+        u: Response = post(url, data=bytesFile.raw, headers=headers)
+        bytesFile.close()
+    return u
 
 
 def main() -> None:
