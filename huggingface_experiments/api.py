e4740c81cba382147cbc888e8b0f82c187e863c6
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-06-07 14:40:53 -0500

    Add code to delete a repo

--- huggingface_experiments/api.py
+++ huggingface_experiments/api.py
@@ -22,10 +22,23 @@ def createModelRepo(token: str, name: str, organization: str = None) -> Response
     p: Response = post(url, headers=headers, json=json)
     return p
 
+def deleteModelRepo(token: str, name: str, organization: str = None) -> Response:
+    url: str = f"{rootURL}/api/repos/delete"
+    headers: dict = {"authorization": f"Bearer {token}"}
+
+    match organization:
+        case None:
+            json: dict = {"name": name}
+        case _:
+            json: dict = {"name": name, "organization": organization}
+
+    p: Response = post(url, headers=headers, json=json)
+    return p
 
 def main() -> None:
     args: Namespace = apiArgs()
-    print(createModelRepo(token=args.admin_token, name="test2"))
+    print(createModelRepo(token=args.admin_token, name="test1").status_code)
+    print(deleteModelRepo(token=args.admin_token, name="test1").status_code)
 
 
 if __name__ == "__main__":
